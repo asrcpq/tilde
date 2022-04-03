0af2462b30efd5a76c60fbf2e10bfc2aa3064977
asrcpq <asrcpq@outlook.com>
2022-04-03 14:58:40 +0000

    fix setup.py

--- setup.py
+++ setup.py
@@ -1,13 +1,10 @@
-from setuptools import setup
+from setuptools import setup, find_packages
 
 setup(name='tilde',
       version='0.1',
-      packages='tilde',
+      packages=find_packages(),
       entry_points={
           'console_scripts': [
               'tilde=tilde.main:main'
-
           ]
-
       }, install_requires=['problog', 'scikit-learn', 'pyswip'])
-
